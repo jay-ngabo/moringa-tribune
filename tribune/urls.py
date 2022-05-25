16c40d6af179328170ac2ea632992cdee93e0680
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-25 10:57:54 +0300

    add all changes

--- tribune/urls.py
+++ tribune/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^news/',include('news.urls')),
+    url(r'',include('news.urls')),
     # url(r'^today/',include('today.urls'))
     
 ]
