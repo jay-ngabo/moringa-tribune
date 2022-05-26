ab18664b8a3df4bb5e6d18836db949731b7d7c7c
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-26 07:55:45 +0300

    add all changes

--- news/views.py
+++ news/views.py
@@ -41,16 +41,20 @@ def past_days_news(request,past_date):
     return HttpResponse(html)
 
 
-def past_days_news(request,past_date):
-
+def past_days_news(request, past_date):
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-    return render(request, 'all-news/past-news.html', {"date": date})
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
 
 # date
 
